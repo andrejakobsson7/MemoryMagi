616d37e8f71231a73c9035348dfbf9eefc333ad8
Tim Nordin <74505667+Sidus-96@users.noreply.github.com>
2024-08-21 17:54:05 +0200

    useritem

--- MemoryMagi/Controllers/UserItemController.cs
+++ MemoryMagi/Controllers/UserItemController.cs
@@ -15,6 +15,26 @@ namespace MemoryMagi.Controllers
             _userItemRepository = userItemRepository;
         }
 
+
+        [HttpGet(Name = "GetAllUserItemsByUserId")]
+        public async Task<IActionResult> GetAllItems(string userId)
+        {
+            // Get the UserItems that match the given UserId
+            List<UserItem> userItems = await _userItemRepository.GetUserItemsByUserId(userId);
+
+            if (userItems == null || !userItems.Any())
+            {
+                return NotFound();  // Use NotFound if no items are found
+            }
+
+            // Extract the ItemIds from the UserItems
+            List<int> itemIds = userItems.Select(ui => ui.ItemId).ToList();
+
+            // Return the list of ItemIds
+            return Ok(itemIds);
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> AddUserItemsAsync(List<UserItem> userItems)
         {
--- MemoryMagi/Repositories/IUserItemRepository.cs
+++ MemoryMagi/Repositories/IUserItemRepository.cs
@@ -1,5 +1,6 @@
 ﻿using MemoryMagi.Database;
 using MemoryMagi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MemoryMagi.Repositories
 {
@@ -8,5 +9,12 @@ namespace MemoryMagi.Repositories
         public AppDbContext _context { get; set; }
 
         public Task<List<UserItem>> AddUserItemsAsync(List<UserItem> userItems);
+
+        public async Task<List<UserItem>> GetUserItemsByUserId(string userId)
+        {
+            return await _context.UserItems
+                                 .Where(ui => ui.UserId == userId)
+                                 .ToListAsync();
+        }
     }
 }
