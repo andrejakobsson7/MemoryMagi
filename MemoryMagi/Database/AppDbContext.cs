6cc493a3250756dff56e84c6ae7b5be8ab6f3b48
Tim Nordin <74505667+Sidus-96@users.noreply.github.com>
2024-09-16 21:59:34 +0200

    Lagt till url länkar för kategoribilderna

--- MemoryMagi/Database/AppDbContext.cs
+++ MemoryMagi/Database/AppDbContext.cs
@@ -167,19 +167,19 @@ namespace MemoryMagi.Database
                 {
                     Id = 1,
                     Name = "Frukt",
-                    Image = "images/Veteran_car.jpg"
+                    Image = "https://i.dietdoctor.com/se/wp-content/2019/10/Fruits-and-vegetables-guide.jpg?auto=compress%2Cformat&w=1200&h=800&fit=crop"
                 },
                 new CategoryModel()
                 {
                     Id = 2,
                     Name = "Djur",
-                    Image = "images/Veteran_car.jpg"
+                    Image = "https://img.pixers.pics/pho_wat(s3:700/FO/57/06/50/45/700_FO57065045_3947f9ea6db6ffee13e8d6526c4b092f.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/posters-vilda-djur-pa-cartoon-jungle-jungle-vilda-djur.jpg.jpg"
                 },
                 new CategoryModel()
                 {
                     Id = 3,
                     Name = "Bilar",
-                    Image = "images/Veteran_car.jpg"
+                    Image = "https://sohu-shop.se/images/wallstickers/wallstickers-med-catoon-biler.webp"
                 }
             );
 
